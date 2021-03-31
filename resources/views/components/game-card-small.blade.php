c500b89a73b48720117e7286d23545309e4cb478
Marius Lueck <mluecksen@gmail.com>
2021-03-31 15:43:20 +0200

    fixed missing links in small game card

--- resources/views/components/game-card-small.blade.php
+++ resources/views/components/game-card-small.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ $game['coverUrl'] }}" alt="game cover" class="w-16 hover:opacity-75 transition ease-in-out duration-150">
     </a>
     <div class="ml-4">
-        <a href="#" class="hover:text-gray-300">{{ $game['name'] }}</a>
+        <a href="{{ route('games.show', ['slug' => $game['slug']]) }}" class="hover:text-gray-300">{{ $game['name'] }}</a>
         <div class="text gray-400 text-sm mt-1">{{ $game['releaseDate'] }}</div>
     </div>
 </div>
